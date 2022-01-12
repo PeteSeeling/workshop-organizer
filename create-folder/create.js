ae243fdc338463060ce5878d829913fc5661b26b
Pete Seeling <peteseeling@gmail.com>
2022-01-11 18:05:46 -0600

    create page dom grabbed

---
+++ create-folder/create.js
@@ -0,0 +1,5 @@
+
+
+const participantForm = document.querySelector('#participant-form');
+const addButton = document.querySelector('#add-participant');
+const dropdown = document.querySelector('workshop-id');
